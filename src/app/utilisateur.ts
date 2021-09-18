d2991533e571b416a5c97702fa9e7c30118771cd
Olfa-Babai <olfa.babai@esprit.tn>
2021-09-18 20:25:44 +0200

    Initial commit

---
+++ src/app/utilisateur.ts
@@ -0,0 +1,15 @@
+
+export class Utilisateur{
+    id : number;
+    nom : string;
+    prenom : string;
+    adresseEmail : string;
+    mdp : string;
+
+    constructor(
+        nom:string,
+        prenom:string,
+        adresseEmail:string,
+        mdp:string){}
+        
+}
